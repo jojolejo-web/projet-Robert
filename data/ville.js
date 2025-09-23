46b1bbf29356bdf932520b3e8cfe9bc5bafb293e
Luffyy321 <clemv490@gmail.com>
2025-09-23 16:18:52 +0200

    Image

--- data/ville.js
+++ data/ville.js
@@ -3,66 +3,105 @@ const ville = [
      
     name: "Île-de-France",
     description: "Région autour de Paris, riche en patrimoine historique et culturel.",
-    activité: "Visiter musées et monuments (Louvre, Tour Eiffel), balades en ville"
+    activité: "Visiter musées et monuments (Louvre, Tour Eiffel), balades en ville",
+    img1:" https://i.pinimg.com/1200x/66/1c/ab/661cab7438c28bc1c6d3f8ca28276881.jpg",
+    img2: "https://i.pinimg.com/736x/9d/a9/e6/9da9e619b92fde1f455b0a6fea5ef3c2.jpg",
+    img3:
   },
   {
     name: "Auvergne-Rhône-Alpes",
     description: "Paysages variés : montagnes (Alpes), volcans, grandes villes comme Lyon.",
-    activité: "Ski, randonnées en montagne, gastronomie lyonnaise"
+    activité: "Ski, randonnées en montagne, gastronomie lyonnaise",
+    img1: "",
+    img2: "",
+    img3:
   },
   {
     name: "Bourgogne-Franche-Comté",
     description: "Terroir renommé, vignobles et patrimoine médiéval.",
-    activité: "Dégustation de vins, visites de châteaux et canaux"
+    activité: "Dégustation de vins, visites de châteaux et canaux",
+    img1: "",
+    img2: "",
+    img3:
   },
   {
     name: "Bretagne",
     description: "Côtes découpées, traditions maritimes et fortes identités locales.",
-    activité: "Balades côtières, fruits de mer, festivals celtiques"
+    activité: "Balades côtières, fruits de mer, festivals celtiques",
+    img1: "",
+    img2: "",
+    img3:
   },
   {
     name: "Centre-Val de Loire",
     description: "Région des châteaux de la Loire et paysages agricoles.",
-    activité: "Visiter châteaux, balade à vélo le long de la Loire"
+    activité: "Visiter châteaux, balade à vélo le long de la Loire",
+    img1: "",
+    img2: "",
+    img3:
   },
   {
     name: "Corse",
     description: "Île méditerranéenne avec montagnes intérieures et plages sublimes.",
-    activité: "Randonnée (GR20), plages, découverte de la gastronomie corse"
+    activité: "Randonnée (GR20), plages, découverte de la gastronomie corse",
+    img1: "",
+    img2: "",
+    img3:
   },
   {
     name: "Grand Est",
     description: "Région frontalière à l’histoire riche, vignobles d’Alsace et villes historiques.",
-    activité: "Visiter marchés de Noël, déguster vins d'Alsace, découvrir Strasbourg"
+    activité: "Visiter marchés de Noël, déguster vins d'Alsace, découvrir Strasbourg",
+    img1: "",
+    img2: "",
+    img3:
   },
   {
     name: "Hauts-de-France",
     description: "Nord de la France : histoire industrielle, plages et villes comme Lille.",
-    activité: "Patrimoine industriel, plages, gastronomie du Nord"
+    activité: "Patrimoine industriel, plages, gastronomie du Nord",
+    img1: "",
+    img2: "",
+    img3:
   },
   {
     name: "Normandie",
     description: "Côtes, bocage, plages du Débarquement et produits laitiers célèbres.",
-    activité: "Découvrir plages du Débarquement, cidre, fromages"
+    activité: "Découvrir plages du Débarquement, cidre, fromages",
+    img1: "",
+    img2: "",
+    img3:
   },
   {
     name: "Nouvelle-Aquitaine",
     description: "Grande région aux vignobles (Bordeaux), côtes atlantiques et forêts.",
-    activité: "Surf, dégustation de vins, visites de Bastides"
+    activité: "Surf, dégustation de vins, visites de Bastides",
+    img1: "",
+    img2: "",
+    img3:
   },
   {
     name: "Occitanie",
     description: "Du littoral méditerranéen aux montagnes des Pyrénées, riche patrimoine occitan.",
-    activité: "Plages, randonnées pyrénéennes, visites historiques (Carcassonne)"
+    activité: "Plages, randonnées pyrénéennes, visites historiques (Carcassonne)",
+    img1: "",
+    img2: "",
+    img3:
   },
   {
     name: "Pays de la Loire",
     description: "Côte atlantique, vignobles et villes dynamiques comme Nantes.",
-    activité: "Balades en bord d'eau, châteaux, festivals"
+    activité: "Balades en bord d'eau, châteaux, festivals",
+    img1: "",
+    img2: "",
+    img3:
   },
   {
     name: "Provence-Alpes-Côte d'Azur",
     description: "Littoral méditerranéen, montagnes et patrimoine provençal.",
-    activité: "Plages, marchés provençaux, randonnées et stations de ski proches"
+    activité: "Plages, marchés provençaux, randonnées et stations de ski proches",
+    img1: "",
+    img2: "",
+    img3:
   }
 ]
