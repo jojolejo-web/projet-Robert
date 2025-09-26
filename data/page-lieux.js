ab95e45b8e7db413b57219dc70ef8209e25a41a1
TristanMrt <tristanmiramont14@gmail.com>
2025-09-26 11:53:49 +0200

    data-lieux

---
+++ data/page-lieux.js
@@ -0,0 +1,13 @@
+const lieux = [
+
+    {
+        top:1,
+        nomLieu:"Gouffre de Padirac",
+        nomVille:"46500 Padirac",
+        img: "",
+        text:"c'est cool le gouffre de Padirac en vrai.",
+    },
+    {
+
+    }
+]
