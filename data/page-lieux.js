617e5bd4af22ef47ebf6bbd463a47fd2aaeef517
TristanMrt <tristanmiramont14@gmail.com>
2025-09-30 15:35:46 +0200

    link data lieux

--- data/page-lieux.js
+++ data/page-lieux.js
@@ -8,6 +8,66 @@ const lieux = [
         text:"c'est cool le gouffre de Padirac en vrai.",
     },
     {
-
+        top:1,
+        nomLieu:"Gouffre de Padirac",
+        nomVille:"46500 Padirac",
+        img: "",
+        text:"c'est cool le gouffre de Padirac en vrai.",
+    },
+    {
+        top:1,
+        nomLieu:"Gouffre de Padirac",
+        nomVille:"46500 Padirac",
+        img: "",
+        text:"c'est cool le gouffre de Padirac en vrai.",
+    },
+    {
+        top:1,
+        nomLieu:"Gouffre de Padirac",
+        nomVille:"46500 Padirac",
+        img: "",
+        text:"c'est cool le gouffre de Padirac en vrai.",
+    },
+    {
+        top:1,
+        nomLieu:"Gouffre de Padirac",
+        nomVille:"46500 Padirac",
+        img: "",
+        text:"c'est cool le gouffre de Padirac en vrai.",
+    },
+    {
+        top:1,
+        nomLieu:"Gouffre de Padirac",
+        nomVille:"46500 Padirac",
+        img: "",
+        text:"c'est cool le gouffre de Padirac en vrai.",
+    },
+    {
+        top:1,
+        nomLieu:"Gouffre de Padirac",
+        nomVille:"46500 Padirac",
+        img: "",
+        text:"c'est cool le gouffre de Padirac en vrai.",
+    },
+    {
+        top:1,
+        nomLieu:"Gouffre de Padirac",
+        nomVille:"46500 Padirac",
+        img: "",
+        text:"c'est cool le gouffre de Padirac en vrai.",
+    },
+    {
+        top:1,
+        nomLieu:"Gouffre de Padirac",
+        nomVille:"46500 Padirac",
+        img: "",
+        text:"c'est cool le gouffre de Padirac en vrai.",
+    },
+    {
+        top:1,
+        nomLieu:"Gouffre de Padirac",
+        nomVille:"46500 Padirac",
+        img: "",
+        text:"c'est cool le gouffre de Padirac en vrai.",
     }
 ]
